d4383e977881657fb8b08273c84c3ce50303738f
Matheus Araújo Akiyoshi Loureiro <matheus.loureiro@ainetw.com>
2023-06-06 19:24:52 -0300

    Layer

--- Background.h
+++ Background.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "Ente.h"
+#include "Layer.h"
+#include "IDs.h"
+
+
 class Background :
     public Ente
 {
 
+private:
+    sf::Vector2f lastCameraPos;
+    std::vector<Layer*> layer;
 public:
     Background(const IDs ID);
     ~Background();
